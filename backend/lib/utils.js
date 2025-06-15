1fd05f5c3fb710f867f12167291d58231159f40f
Himanshu jangid <jangidhimanshu2000@gmail.com>
2025-06-15 16:06:49 +0530

    Update utils.js

--- backend/lib/utils.js
+++ backend/lib/utils.js
@@ -7,10 +7,10 @@ export const generateToken = (userId, res, role) => {
 
     res.cookie("jwt", token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000,
-    }); 
+        secure: true,
+        sameSite: 'None',
+        maxAge: 24 * 60 * 60 * 1000,
+    });
 
     return token;
 };
